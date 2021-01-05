f4cec272fcd389aca0f3024810952b43b69cb624
Satya.Tejadvs <sathyteja.dvs@trikatechnologies.com>
2021-01-05 13:56:24 +0530

    Decorated the store_credit_card and credit_card models.   - Created the secrets.yml file to make use the netcentrix credentials.   - Initialized the gateway.rb file.

---
+++ config/initializers/workarea.rb
@@ -0,0 +1,5 @@
+Workarea::NetcentrixPaymentGateway.auto_initialize_gateway
+
+Workarea.configure do |config|
+  config.gateways.usa_epay = 'Workarea::Netcentrix::Gateway'
+end
--- lib/workarea/netcentrix_payment_gateway.rb
+++ lib/workarea/netcentrix_payment_gateway.rb
@@ -1,7 +1,36 @@
 require "workarea/netcentrix_payment_gateway/engine"
+require 'netcentrix/gateway'
 
 module Workarea
   module NetcentrixPaymentGateway
     # Your code goes here...
+    def self.credentials
+      return {} unless Rails.application.secrets.netcentrix_payment_gateway.present?
+      Rails.application.secrets.netcentrix_payment_gateway.symbolize_keys
+    end
+
+    # Conditionally use the real gateway when secrets are present.
+    # Otherwise, use the bogus gateway.
+    #
+    # @return [ActiveMerchant::Billing::Gateway]
+    def self.gateway
+      Workarea.config.gateways.credit_card
+    end
+
+    def self.gateway=(gateway)
+      Workarea.config.gateways.credit_card = gateway
+    end
+
+    def self.auto_initialize_gateway
+      if credentials.present?
+        if Rails.env.test?
+          self.gateway = ActiveMerchant::Billing::BogusCreditCardGateway.new
+        else
+          self.gateway = ActiveMerchant::Billing::NetcentrixPaymentGateway.new credentials
+        end
+      else
+        self.gateway = ActiveMerchant::Billing::BogusCreditCardGateway.new
+      end
+    end
   end
 end
