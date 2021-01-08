0b5241745b610231a9fbc9e33f24295df38e13a4
Satya.Tejadvs <sathyteja.dvs@trikatechnologies.com>
2021-01-08 12:47:34 +0530

    Copied all the mayank netcentrix api code to the netcentrix gateway file.

--- lib/active_merchant/billing/gateways/netcentrix_payment_gateway.rb
+++ lib/active_merchant/billing/gateways/netcentrix_payment_gateway.rb
@@ -1,15 +1,16 @@
+require 'net/http'
+require 'httparty'
 
 module ActiveMerchant
   module Billing
     class NetcentrixPaymentGateway < CreditCard
       attr_reader :options
 
-      AUTHORIZATION = '53433'
-      SUCCESS_MESSAGE = 'Netcentrix Gateway: Forced success'
+      SUCCESS_MESSAGE = "Netcentrix Gateway: Payment is successfully processed."
      
       def authorize(money, paysource, options = {})
         payment_info = order_info(money, paysource)
-        Workarea::Netcentrix::NetcentrixGateway.new.payment_api(payment_info)
+        payment_api(payment_info, money)
       end
 
       def void(paysource, options = {})
@@ -25,11 +26,71 @@ module ActiveMerchant
           "CardNumber" => "#{paysource.number}",
           "Token" => '',
           "CardType" => "VI",
-          "CardExpMonth" => "#{paysource.month}",
+          "CardExpMonth" => "01",
           "CardExpYear" => "#{paysource.year}",
           "CardCID" => "#{paysource.verification_value}"
         }
         return order
+      end
+
+      def headers
+        {
+          "Content-Type" => "text/xml"
+        }
+      end
+
+      def payment_authentication
+        {
+          "merchant" => "#{Rails.application.secrets.netcentrix_payment_merchant rescue nil}" ,
+          "password" => "#{Rails.application.secrets.netcentrix_payment_password rescue nil}" 
+        }
+      end
+
+      # Method to generate XML body payload for payment API. Method argument to replace necessary xml fields.
+      def payment_body(ord)
+
+        req_body = {
+          "Authentication" => {
+            "Merchant" => payment_authentication['merchant'],
+            "Password" => payment_authentication['password']
+          },
+          "Action" => "PUSHPAYMENT",
+          "Payments" => {
+            "Payment" => ord
+          }
+        }
+        xml_req = req_body.to_xml(:root => 'DATASTART', skip_types: true)
+      end
+
+      # API call to push payment related information to the ecometry via netcentrix APIs. In order to successfully hit the API please pass payment related information in the method argument which is in Hash structure.
+      def payment_api(order, money)
+        body = payment_body(order)
+        endpoint = Rails.application.secrets.netcentrix_api rescue nil
+        response = HTTParty.post(endpoint, :headers => headers, :body => body)
+        hsh = Hash.from_xml(response.parsed_response)
+
+        if hsh['Pushpayment']['Success'] == 'Y'
+          puts "Payment is successfully processed."
+          Response.new(
+            success_from(response),
+            SUCCESS_MESSAGE,
+            {authorized_amount: money},
+            authorization: authorization_from(response),
+          )
+        else
+          response = false
+          puts "Bad Request please validate information."
+        end
+      end
+
+      private
+
+      def success_from(response)
+        response.code == 200
+      end
+
+      def authorization_from(response)
+        response.object_id.to_s
       end
     end
   end
