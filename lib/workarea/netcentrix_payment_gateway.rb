88db669d07bb623feb56d7cf305c861de498adb1
Gunasekaran Raja <guna.r@trikatechnologies.com>
2021-01-11 15:19:31 +0530

    Don't use netcentrix for testing

--- lib/workarea/netcentrix_payment_gateway.rb
+++ lib/workarea/netcentrix_payment_gateway.rb
@@ -7,10 +7,7 @@ module Workarea
   module NetcentrixPaymentGateway
 
     def self.auto_initialize_gateway
-      if Rails.env.test?
-        Workarea.config.gateways.credit_card = ActiveMerchant::Billing::BogusGateway.new
-        ActiveMerchant::Billing::Base.mode = :test
-      else
+      unless Rails.env.test?
         Workarea.config.gateways.credit_card = ActiveMerchant::Billing::NetcentrixPaymentGateway.new
       end
     end
